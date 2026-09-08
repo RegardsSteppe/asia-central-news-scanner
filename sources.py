d67a65ebb25775483a76d3533814981484b31469
RegardsSteppe <RegardsSteppe@proton.me>
2026-09-08 11:53:24 +0200

    simplify list of sources for basic tests

--- sources.py
+++ sources.py
@@ -28,8 +28,8 @@ SOURCES = [
         ],
 
         "max_articles": 150,
-    },
-
+    }]
+'''
     {
         "name": "Eurasianet",
         "short_name": "Eurasianet",
@@ -555,3 +555,4 @@ SOURCE_PROFILES = {
         ),
     },
 }
+'''
